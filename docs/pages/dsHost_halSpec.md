60bd5054510c581c79c6d8c528d20b5ff489cf9b
hgfell683 <107510770+hgfell683@users.noreply.github.com>
2023-08-23 12:19:37 -0400

    RDK-43735 - Provide documentation for component "DS Host HAL"
    Reason for change:
    Documentation Update for dsHost_halSpec.md and dsHost.h
    Test Procedure: None
    Risks: Low
    Priority: P1
    
    Signed-off-by: Gfeller, Hayden (Contractor) Hayden_Gfeller@comcast.com

---
+++ docs/pages/dsHost_halSpec.md
@@ -0,0 +1,191 @@
+# DEVICE SETTINGS HAL Documentation
+
+## Version History
+
+| Date(DD/MM/YY) | Comment | Version |
+| ---- | ------- | ------- |
+| 13/03/23 | Edit  | 1.0.1 |
+| 20/04/23 | First Release | 1.0.0 |
+
+## Table of Contents
+
+- [Acronyms, Terms and Abbreviations](#acronyms-terms-and-abbreviations)
+- [Description](#description)
+- [Component Runtime Execution Requirements](#component-runtime-execution-requirements)
+  - [Initialization and Startup](#initialization-and-startup)
+  - [Threading Model](#threading-model)
+  - [Process Model](#process-model)
+  - [Memory Model](#memory-model)
+  - [Power Management Requirements](#power-management-requirements)
+  - [Asynchronous Notification Model](#asynchronous-notification-model)
+  - [Blocking calls](#blocking-calls)
+  - [Internal Error Handling](#internal-error-handling)
+  - [Persistence Model](#persistence-model)
+- [Non-functional requirements](#non-functional-requirements)
+  - [Logging and debugging requirements](#logging-and-debugging-requirements)
+  - [Memory and performance requirements](#memory-and-performance-requirements)
+  - [Quality Control](#quality-control)
+  - [Licensing](#licensing)
+  - [Build Requirements](#build-requirements)
+  - [Variability Management](#variability-management)
+  - [Platform or Product Customization](#platform-or-product-customization)
+- [Interface API Documentation](#interface-api-documentation)
+  - [Theory of operation and key concepts](#theory-of-operation-and-key-concepts)
+  - [Diagrams](#diagrams)
+
+## Acronyms, Terms and Abbreviations
+
+- `HAL`    - Hardware Abstraction Layer
+- `API`    - Caller Programming Interface
+- `Caller` - Any user of the interface via the `API`s
+- `CPU`    - Central Processing Unit
+- `DS`:      Device Settings.
+- `HAL`:     Hardware Abstraction Layer.
+- `EDID`:    Extended Display Information Data.
+- `CPU`:     Central Processing Unit
+- `SoC`:     System on chip
+
+## Description
+
+The diagram below describes a high-level software architecture of the module stack.
+
+```mermaid
+%%{ init : { "theme" : "forest", "flowchart" : { "curve" : "linear" }}}%%
+flowchart TD
+y[Caller]<-->x[DEVICE SETTINGS HOST HAL];
+x[DEVICE SETTINGS HOST HAL]<-->z[SOC Drivers];
+style y fill:#99CCFF,stroke:#333,stroke-width:0.3px,align:left
+style z fill:#fcc,stroke:#333,stroke-width:0.3px,align:left
+style x fill:#9f9,stroke:#333,stroke-width:0.3px,align:left
+ ```
+
+DeviceSettings Host `HAL` provides a set of `APIs` to initialize, set, and query information about the `HAL` and `SoC`.
+
+- The main purpose is to facilitate communication between the `Caller`, and `HAL` interface, such that information about the current `HAL` version number, the Host EDID number, the current CPU temperature, and the SoC ID can be queried by the `Caller`.
+
+## Component Runtime Execution Requirements
+
+The component should manage system resources appropriately to avoid memory leaks and excessive resource utilization. Efficient memory management and resource cleanup are essential for stable and reliable execution. Additionally, the component should meet specified performance requirements, including response time, throughput, and resource usage based on the underlying platform's capabilities. The component should also be designed to scale effectively with increased load, being able to handle higher levels of usage without significant degradation in performance or stability.
+
+Failure to meet these requirements will likely result in undefined and unexpected behavior.
+
+### Initialization and Startup
+
+`Caller` must be initialized by calling `dsHostInit()` before calling any other `APIs`. The `Caller` is expected to have complete control over the life cycle of the `DeviceSettings Host` module.
+
+### Threading Model
+
+This interface is not required to be thread safe. Any `caller` invoking the `API`s should ensure calls are made in a thread safe manner. `HAL` is allowed to create internal threads for its operations without excessively consuming system resources. Any threads created by the `HAL` should be handled gracefully and respective error codes should be returned if any corresponding `API` fails.
+
+### Process Model
+
+This interface is required to support a single instantiation with a single process.
+
+### Memory Model
+
+This interface is not required to allocate any memory. Any pointers created by the interface must be cleaned up upon termination.
+
+### Power Management Requirements
+
+Although this interface is not required to be involved in any of the power management operations, the state transitions MUST not affect its operation. e.g. on resumption from a low power state, the interface should operate as if no transition has occurred.
+
+### Asynchronous Notification Model
+
+This interface is not required to support asynchronous notification.
+
+### Blocking calls
+
+There are no blocking calls. Synchronous calls should complete within a reasonable time period.
+
+### Internal Error Handling
+
+All the `API`s must return error synchronously as a return argument. `HAL` is responsible for handling system errors (e.g. out of memory) internally.
+
+### Persistence Model
+
+There is no requirement for the interface to persist any setting information. `Caller` is responsible to persist any settings related to the `HAL`.
+
+## Non-functional requirements
+
+The following non-functional requirements will be supported by the module.
+
+### Logging and debugging requirements
+
+This interface is required to support DEBUG, INFO and ERROR messages. INFO and DEBUG should be disabled by default and enabled when required.
+
+### Memory and performance requirements
+
+This interface will ensure optimal use of memory and CPU according to the specific capabilities of the system.
+
+### Quality Control
+
+- This interface is required to perform static analysis, our preferred tool is Coverity.
+- Have a zero-warning policy with regards to compiling. All warnings are required to be treated as error.
+- Copyright validation is required to be performed, e.g.: Black duck, and FossID.
+- Use of memory analysis tools like Valgrind are encouraged, to identify leaks/corruptions.
+- `HAL` Tests will endeavour to create worst case scenarios to assist investigations.
+- Improvements by any party to the testing suite are required to be fed back.
+
+### Licensing
+
+The `HAL` implementation is expected to released under the Apache License 2.0. 
+
+### Build Requirements
+
+The source code must build into a shared library for Device Settings as Host is apart of Device Settings and must be named as `libdshal.so`. The build mechanism must be independent of Yocto.
+ 
+### Variability Management
+
+- Any changes in the `APIs` should be reviewed and approved by the component architects.
+- `DeviceSettings Host` `HAL` modification should support backward compatibility for the generic operations like image upgrade and downgrade
+- `DeviceSettings Host` should return the dsERR_OPERATION_NOT_SUPPORTED error code, If any of the interface - `APIs` are not supported by the underlying hardware
+- Providers of the `DeviceSettings Host` `HAL` should keep a well-defined version history for tracking alterations across diverse library versions, along with their corresponding verification results.
+
+### Platform or Product Customization
+
+None
+
+## Interface API Documentation
+
+`API`s documentation will be provided by Doxygen which will be generated from the header files.
+
+### Theory of operation and key concepts
+
+The `caller` is expected to have complete control over the life cycle of the `HAL`.
+
+1. Initialize the `HAL` using function: `dsHostInit()` before making any other `API`s calls.  If `dsHostInit()` call fails, the `HAL` must return the respective error code, so that the `caller` can retry the operation.
+
+2. The `caller` can call `dsGetCPUTemperature()`, `dsGetHostEDID()` and `dsGetSocIDFromSDK()` to query the needed information.
+
+3. De-initialized the `HAL` using the function: `dsHostTerm()`
+
+### Diagrams
+
+#### Operational Call Sequence
+
+```mermaid
+%%{ init : { "theme" : "default", "flowchart" : { "curve" : "stepBefore" }}}%%
+   sequenceDiagram
+    participant Caller as Caller
+    participant HAL as DEVICE SETTINGS HOST HAL
+    participant Driver as SoC
+    Caller->>HAL:dsHostInit()
+    Note over HAL: SOC initializes the underlying subsystems.
+    HAL-->>Caller:return
+    Caller->>HAL:dsGetCPUTemperature()
+    Note over HAL: Returns the current CPU temp.
+    HAL->>Driver:Getting the current CPU temp
+    Driver-->>HAL:return
+    Caller->>HAL:dsGetSocIDFromSDK()
+    Note over HAL: Returns the SoC ID.
+    HAL->>Driver:Getting the SoC ID.
+    Driver-->>HAL:return
+    HAL-->>Caller:return
+    Caller->>HAL:dsGetHostEDID()
+    Note over HAL: Returns the Host EDID.
+    HAL->>Driver:Getting the Host EDID.
+    Driver-->>HAL:return
+    HAL-->>Caller:return
+    Caller ->>HAL:dsHostTerm()
+    HAL-->>Caller:return
+ ```
--- include/dsHost.h
+++ include/dsHost.h
@@ -24,30 +24,43 @@
  * <b> Following abbreviations present in HAL API </b>
  *
  * @par Abbreviations
- * - BE:      Big-Endian.
  * - cb:      Callback function (suffix).
  * - DS:      Device Settings.
- * - FPD:     Front-Panel Display.
  * - HAL:     Hardware Abstraction Layer.
- * - LE:      Little-Endian.
- * - LS:      Least Significant.
- * - MBZ:     Must be zero.
- * - MS:      Most Significant.
- * - RDK:     Reference Design Kit.
- * - _t:      Type (suffix).
- * - HDCP:    High-bandwidth Digital Copy Protection.
- * - DTCP:    Digital Transmission Copy Protection.
  * - EDID:    Extended Display Information Data.
+ * - CPU:     Central Processing Unit
+ * - SoC:     System on chip
  * 
  * @ingroup DSSETTINGS_HAL
  */
 
 /**
-* @defgroup devicesettings Device Settings
-* @{
-* @defgroup hal Device Settings HAL
-* @{
-**/
+ * @addtogroup HPK HPK
+ * @{
+ * @par The Hardware Porting Kit
+ * HPK is the next evolution of the well-defined Hardware Abstraction Layer
+ * (HAL), but augmented with more comprehensive documentation and test suites
+ * that OEM or SOC vendors can use to self-certify their ports before taking
+ * them to RDKM for validation or to an operator for final integration and
+ * deployment. The Hardware Porting Kit effectively enables an OEM and/or SOC
+ * vendor to self-certify their own Video Accelerator devices, with minimal RDKM
+ * assistance.
+ *
+ */
+
+/** @addtogroup DS_Manager_HAL DS Manager Hal
+ * @par Application API Specification
+ * Described herein are the DeviceSettings HAL types and functions that are part of
+ * the Host subsystem. The Host subsystem manages system-specific HAL operations.
+ *  @{
+ */
+
+/** @defgroup DSHAL_HOST_API Device Settings HAL Host Public API
+ *
+ *
+ *  @{
+ */
+
 
 #ifndef _DS_HOST_H_
 #define _DS_HOST_H_
@@ -61,234 +74,104 @@ extern "C" {
 
 #define EDID_DATA_SIZE 256
 #define EDID_MAX_DATA_SIZE 512
-  
-/** @addtogroup DSHAL_HOST_API Device Settings HAL Host Public API
- *  @ingroup devicesettingshalapi
- *
- *  Described herein are the DeviceSettings HAL types and functions that are part of
- *  the Host subsystem. The Host subsystem manages system-specific hardware operations.
- *
- *  @{
- */
 
 /**
- * @brief Initialize the Host sub-system.
+ * @brief Initialize the Host HAL.
+ * 
+ * This function has to initialize any needed resources within the module.
  *
- * This function must initialize all the system specific drivers to be initialized  prior
- * to any driver specific calls.
- *
- * @return dsError_t - Device Settings error code
- * @retval dsERR_NONE Indicates the call was successful.
- * @retval dsERR_GENERAL Indicates error due to general failure. In the HAL side implementation, all of the return values will
- * be initialized with this error code. So that any of the undefined error case scenario in the HAL code, will be report as this error code.
+ * @return dsError_t                   - Status
+ * @retval dsERR_NONE                   - Success
+ * @retval dsERR_ALREADY_INITIALIZED    - Function is already initialized.
+ * @retval dsERR_GENERAL                - Underlying undefined platform error
+ * 
  * @warning  This API is Not thread safe.
  * @see dsHostTerm()
+ * 
+ * 
  */
 
 dsError_t dsHostInit();
 
 /**
- * @brief Set the power mode.
- *
- * This function sets the power mode of the host to active or standby and turns on/off
- * all the ouput ports.
- *
- * @param[in] newPower    The power mode of the host (::dsPOWER_STANDBY or ::dsPOWER_ON)
- *
- * @return dsError_t - Device Settings error code
- * @retval dsERR_NONE Indicates the call was successful.
- * @retval dsERR_INVALID_PARAM Indicates error due to invalid parameter value.
- * @retval dsERR_INVALID_STATE Indicates the respective api is called with out calling  Init or  preceding Init has failed
- * @retval dsERR_GENERAL Indicates error due to general failure. In the HAL side implementation, all of the return values will
- * be initialized with this error code. So that any of the undefined error case scenario in the HAL code, will be report as this error code.
- *
- * @note dsPOWER_OFF is not currently being used.
- * @pre dsHostInit() should be called before calling this API.
- * @warning  This API is Not thread safe.
- * @see dsGetHostPowerMode()
- */
-
-dsError_t dsSetHostPowerMode(int newPower);
-
-/**
- * @brief Get the current power mode.
- *
- * This function gets the current power mode of the host. 
- *
- * @param[out] currPower  current power state of the system.
- *                        mode on return. It returns one of:
- *                              - ::dsPOWER_OFF
- *                              - ::dsPOWER_STANDBY
- *                              - ::dsPOWER_ON
- *
- * @return dsError_t - Device Settings error code
- * @retval dsERR_NONE Indicates the call was successful.
- * @retval dsERR_INVALID_PARAM Indicates error due to invalid parameter value.
- * @retval dsERR_INVALID_STATE Indicates the respective api is called with out calling  Init or  preceding Init has failed
- * @retval dsERR_GENERAL Indicates error due to general failure. In the HAL side implementation, all of the return values will
- * be initialized with this error code. So that any of the undefined error case scenario in the HAL code, will be report as this error code.
- * @pre dsHostInit() should be called before calling this API.
- * @warning  This API is Not thread safe.
- * @see dsSetHostPowerMode()
- */
-
-dsError_t dsGetHostPowerMode(int *currPower);
-
-/**
  * @brief Terminate the Host sub-system.
  *
- * This function resets the data structures used within the Host module and releases any
- * handles specific to the host module.
+ * This function has to release all the resources allocated during the init function.
  *
- * @return dsError_t - Device Settings error code
- * @retval dsERR_NONE Indicates the call was successful.
- * be initialized with this value. So that any of the undefined error case scenario in the HAL code, will be report as this error code.
- * @retval dsERR_INVALID_PARAM Indicates error due to invalid parameter value.
- * @retval dsERR_INVALID_STATE Indicates the respective api is called with out calling dsHostInit() or  preceding dsHostInit has failed
- * @retval dsERR_GENERAL Indicates error due to general failure. In the HAL side implementation, all of the return values will
- * be initialized with this error code. So that any of the undefined error case scenario in the HAL code, will be report as this error code.
- * @pre dsHostInit() should be called before calling this API.
+ * @return dsError_t               - Status
+ * @retval dsERR_NONE               - Success 
+ * @retval dsERR_NOT_INITIALIZED    - Module is not initialised
+ * @retval dsERR_GENERAL            - General failure.
+ * 
  * @warning  This API is Not thread safe.
+ * 
  * @see dsHostInit()
+ * 
  */
 
 dsError_t dsHostTerm();
 
 /**
- * @brief This function returns the preferred sleep mode which is persisted.
- *
- * @param[out] pMode      Current preferred sleep mode settings of the device.
- *
- * @return dsError_t - Device Settings error code
- * @retval dsERR_NONE If sucessfully dsGetPreferredSleepMode api has been called.
- * @retval dsERR_INVALID_PARAM Indicates error due to invalid parameter value.
- * @retval dsERR_INVALID_STATE Indicates the respective api is called with out calling dsHostInit() or  preceding dsHostInit has failed
- * @retval dsERR_GENERAL Indicates error due to general failure. In the HAL side implementation, all of the return values will
- * be initialized with this error code. So that any of the undefined error case scenario in the HAL code, will be report as this error code.
- * @pre dsHostInit() should be called before calling this API.
- * @warning  This API is Not thread safe.
- * @see dsSetPreferredSleepMode()
- */
-
-dsError_t dsGetPreferredSleepMode(dsSleepMode_t *pMode);
-
-/**
- * @brief This function sets the preferred sleep mode. This value will persisted across reboots.
- *
- * @param[in] mode        Sleep mode to set.
- *
- * @return dsError_t - Device Settings error code
- * @retval dsERR_NONE If sucessfully dsSetPreferredSleepMode api has been called.
- * @retval dsERR_INVALID_PARAM Indicates error due to invalid parameter value.
- * @retval dsERR_INVALID_STATE Indicates the respective api is called with out calling dsHostInit() or  preceding dsHostInit has failed
- * @retval dsERR_GENERAL Indicates error due to general failure. In the HAL side implementation, all of the return values will
- * be initialized with this error code. So that any of the undefined error case scenario in the HAL code, will be report as this error code.
- * @pre dsHostInit() should be called before calling this API.
- * @warning  This API is Not thread safe.
- * @see dsGetPreferredSleepMode()
- */
-
-dsError_t dsSetPreferredSleepMode(dsSleepMode_t mode);
-
-/**
  * @brief This function gets the CPU temperature in centigrade.
+ * 
+ * @param[out] cpuTemperature   - CPU temperature value returned in centigrade.
  *
- * @param[out] cpuTemperature CPU temperature value is obtained in centigrade 
- *
- * @return dsError_t - Device Settings error code
- * @retval dsERR_NONE If sucessfully dsGetCPUTemperature api has been called.
- * @retval dsERR_INVALID_PARAM Indicates error due to invalid parameter value.
- * @retval dsERR_INVALID_STATE Indicates the respective api is called with out calling dsHostInit() or  preceding dsHostInit has failed
- * @retval dsERR_GENERAL Indicates error due to general failure. In the HAL side implementation, all of the return values will
- * be initialized with this error code. So that any of the undefined error case scenario in the HAL code, will be report as this error code.
- * @pre dsHostInit() should be called before calling this API.
+ * @return dsError_t                        - Status
+ * @retval dsERR_NONE                       - Success
+ * @retval dsERR_NOT_INITIALIZED            - Module is not initialised
+ * @retval dsERR_INVALID_PARAM              - Parameter passed to this function is invalid
+ * @retval dsERR_OPERATION_NOT_SUPPORTED    - The attempted operation is not supported
+ * @retval dsERR_GENERAL                    - Underlying undefined platform error
+ * 
  * @warning  This API is Not thread safe.
+ * 
  */
 
 dsError_t dsGetCPUTemperature(float *cpuTemperature);
 
 /**
- * @brief Get DS HAL API Version.
- *
- * In 4 byte VersionNumber, Two Most significant Bytes are Major number
- * and Two Least Significant Bytes are minor number.
- *
- * @param[out] versionNumber 4 Bytes of version number of DS HAL. In 4 byte VersionNumber, Two Most significant Bytes are Major number 
- * and Two Least Significant Bytes are minor number.
- *
- * @return Device Settings error code
- * @retval dsERR_NONE Successfully got the version number from dsHAL.
- * @retval dsERR_INVALID_PARAM Indicates error due to invalid parameter value.
- * @retval dsERR_INVALID_STATE Indicates the respective api is called with out calling dsHostInit() or  preceding dsHostInit has failed
- * @retval dsERR_GENERAL Indicates error due to general failure. In the HAL side implementation, all of the return values will
- * be initialized with this error code. So that any of the undefined error case scenario in the HAL code, will be report as this error code.
- * @pre dsHostInit() should be called before calling this API.
- * @warning  This API is Not thread safe.
- * @see dsSetVersion()
- */
-
-dsError_t dsGetVersion(uint32_t *versionNumber);
-
-/**
- * @brief Allows the application to set the runtime version of the dsHAL
- *
- * @param[in] versionNumber  4 Bytes of version number of DS HAL.In 4 byte VersionNumber, Two Most significant Bytes are Major number 
- * and Two Least Significant Bytes are minor number.
- *
- * @return dsError_t - Device Settings error code
- * @retval dsERR_NONE Successfully set the version for dsHAL.
- * @retval dsERR_INVALID_PARAM Indicates error due to invalid parameter value.
- * @retval dsERR_INVALID_STATE Indicates the respective api is called with out calling dsHostInit() or  preceding dsHostInit has failed
- * @retval dsERR_GENERAL Indicates error due to general failure. In the HAL side implementation, all of the return values will
- * be initialized with this error code. So that any of the undefined error case scenario in the HAL code, will be report as this error code.
- * @pre dsHostInit() should be called before calling this API.
- * @warning  This API is Not thread safe.
- * @see dsGetVersion()
- */
-
-dsError_t dsSetVersion(uint32_t versionNumber);
-
-/**
  * @brief This function returns SOC ID
  *
- * @param[out] socID        8 byte Chip ID programmed to the CHIP One Time Programmable area.
+ * @param[out] socID    - 8 byte Chip ID programmed to the CHIP One Time Programmable area.
  *
- * @return dsError_t - Device Settings error code
- * @retval dsERR_NONE If sucessfully dsSetPreferredSleepMode api has been called.
- * @retval dsERR_UNKNOWN General failure.
- * @retval dsERR_GENERAL Indicates error due to general failure. In the HAL side implementation, all of the return values will
- * be initialized with this error code. So that any of the undefined error case scenario in the HAL code, will be report as this error code.
- * @pre dsHostInit() should be called before calling this API.
+ * @return dsError_t                        - Status
+ * @retval dsERR_NONE                       - Success
+ * @retval dsERR_NOT_INITIALIZED            - Module is not initialised
+ * @retval dsERR_INVALID_PARAM              - Parameter passed to this function is invalid
+ * @retval dsERR_OPERATION_NOT_SUPPORTED    - The attempted operation is not supported
+ * @retval dsERR_GENERAL                    - Underlying undefined platform error
+ * 
  * @warning  This API is Not thread safe.
+ * 
  */
 
 dsError_t dsGetSocIDFromSDK(char *socID);
 
 /**
- * @brief This function is used to get the host EDID and length
+ * @brief This function is used to get the host EDID and length.
+ * 
+ * The host EDID will be used on devices supporting HDMI input feature.
  *
- * @param[out] edid       host EDID.
- * @param[out] length     length of host EDID
+ * @param[out] edid     - host EDID.
+ * @param[out] length   - length of host EDID. Min value of 0. 
  *
- * @return dsError_t - Device Settings error code
- * @retval dsERR_NONE If sucessfully dsGetHostEDID api has been called.
- * @retval dsERR_INVALID_PARAM Indicates error due to invalid parameter value.
- * @retval dsERR_INVALID_STATE Indicates the respective api is called with out calling dsHostInit() or  preceding dsHostInit has failed
- * @retval dsERR_GENERAL Indicates error due to general failure. In the HAL side implementation, all of the return values will
- * be initialized with this error code. So that any of the undefined error case scenario in the HAL code, will be report as this error code.
- * @pre dsHostInit() should be called before calling this API.
+ * @return dsError_t                        - Status
+ * @retval dsERR_NONE                       - Success
+ * @retval dsERR_NOT_INITIALIZED            - Module is not initialised
+ * @retval dsERR_INVALID_PARAM              - Parameter passed to this function is invalid
+ * @retval dsERR_OPERATION_NOT_SUPPORTED    - The attempted operation is not supported
+ * @retval dsERR_GENERAL                    - Underlying undefined platform error
+ * 
  * @warning  This API is Not thread safe.
+ * 
  */
 
 dsError_t dsGetHostEDID(unsigned char *edid, int *length);
 
-/**
- * @}
- */
+/** @} */ // End of DSHAL_HOST_API doxygen group 
+/** @} */ // End of DS HAL
+/** @} */ // End of HPK
 #ifdef __cplusplus
 }
 #endif
 #endif /* _DS_HOST_H_ */
-
-/** @} */
-/** @} */
