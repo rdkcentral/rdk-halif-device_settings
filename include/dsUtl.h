83e55ed78d4a678ca75c7af02a96a505d6f3d892
hgfell683 <107510770+hgfell683@users.noreply.github.com>
2023-09-15 12:44:49 -0400

    RDK-43735 - Provide documentation for component "DS HdmiIn HAL"
    RDK-43735 - Provide documentation for component "DS HdmiIn HAL"
    
    Reason for change:
    Documentation Update for dsHdmiIn_halSpec.md and dsHdmiIn.h
    Test Procedure: None Risks: Low
    Priority: P1
    
    Signed-off-by: Gfeller, Hayden (Contractor) Hayden_Gfeller@comcast.com

--- include/dsUtl.h
+++ include/dsUtl.h
@@ -55,19 +55,22 @@
  *
  */
 
-/** @addtogroup DeviceSettings_Module DeviceSettings Module
- *  @{
- */
-
-/** @addtogroup DSHAL_UTILS Device Settings HAL Utilities
- *  @ingroup DSHAL_API
- *  @{
- */
+/**
+* @defgroup devicesettings Device Settings HAL
+* @{
+* @defgroup hal Device Settings HAL
+* @{
+**/
 
 #ifndef _DS_UTILS_H_
 #define _DS_UTILS_H_
 
 #include <stdio.h>
+
+/** @addtogroup DSHAL_UTILS Device Settings HAL Utilities
+ *  @ingroup DSHAL_API
+ *  @{
+ */
 
 /**
  * @brief Device Settings general Assert Failure inline definition.
@@ -84,6 +87,103 @@ while(0)
 
 #define dsUTL_DIM(arr) (sizeof((arr))/sizeof((arr)[0]))
 
+/**
+ * @brief Audio output port type validation check.
+ * @see dsAudioPortType_t
+ */
+#define dsAudioType_isValid(t)  (((t) >= dsAUDIOPORT_TYPE_ID_LR ) && ((t) < dsAUDIOPORT_TYPE_MAX))
+
+/**
+ * @brief Audio encoding type validation check.
+ * @see dsAudioEncoding_t
+ */
+#define dsAudioEncoding_isValid(t)  (((t) >= dsAUDIO_ENC_NONE ) && ((t) < dsAUDIO_ENC_MAX))
+
+/**
+ * @brief Audio compression type validation check.
+ * @see dsAudioCompression_t
+ */
+#define dsAudioCompression_isValid(t)  (((t) >= dsAUDIO_CMP_NONE ) && ((t) < dsAUDIO_CMP_MAX))
+
+/**
+ * @brief Audio stereo mode type validation check.
+ * @see dsAudioStereoMode_t
+ */
+#define dsAudioStereoMode_isValid(t)  (((t) >= dsAUDIO_STEREO_UNKNOWN ) && ((t) < dsAUDIO_STEREO_MAX))
+
+/**
+ * @brief Video output port type validation check.
+ * @see dsVideoPortType_t
+ */
+#define dsVideoPortType_isValid(t)  (((t) >= dsVIDEOPORT_TYPE_RF ) && ((t) < dsVIDEOPORT_TYPE_MAX))
+
+/**
+ * @brief Video output resolutions type validation check.
+ * @see dsVideoResolution_t
+ */
+#define dsVideoPortPixelResolution_isValid(t)  (((t) >= dsVIDEO_PIXELRES_720x480 ) && ((t) < dsVIDEO_PIXELRES_MAX))
+
+/**
+ * @brief Video output framerate validation check.
+ * @see dsVideoFrameRate_t
+ */
+#define dsVideoPortFrameRate_isValid(t)  (((t) >= dsVIDEO_FRAMERATE_UNKNOWN ) && ((t) < dsVIDEO_FRAMERATE_MAX))
+
+/**
+ * @brief Video scan mode type validation check.
+ * @see dsVideoScanMode_t
+ */
+#define dsVideoPortScanMode_isValid(t)  (((t) >= dsVIDEO_SCANMODE_INTERLACED ) && ((t) < dsVIDEO_SCANMODE_MAX))
+
+/**
+ * @brief Video Stereo Scopic modes type validation check.
+ * @see dsVideoStereoScopicMode_t
+ */
+#define dsVideoPortStereoScopicMode_isValid(t)  (((t) >= dsVIDEO_SSMODE_UNKNOWN ) && ((t) < dsVIDEO_SSMODE_MAX))
+
+/**
+ * @brief HDCP status validation check.
+ * @see dsHdcpStatus_t
+*/
+#define dsHdcpStatus_isValid(t)  (((t) >= dsHDCP_STATUS_UNPOWERED ) && ((t) < dsHDCP_STATUS_MAX))
+
+/**
+ * @brief Video aspect ratio type validation check.
+ * @see dsVideoAspectRatio_t
+ */
+#define dsVideoPortAspectRatio_isValid(t)  (((t)  >= dsVIDEO_ASPECT_RATIO_4x3 ) && ((t) < dsVIDEO_ASPECT_RATIO_MAX))
+
+/**
+ * @brief Front panel LED colors validation check.
+ * @see dsFPDColor_t
+ */
+#define dsFPDColor_isValid(t)  (((t) & 0xFF000000) == 0)
+
+/**
+ * @brief Front panel LED indicator type validation check.
+ * @see dsFPDIndicator_t
+ */
+#define dsFPDIndicator_isValid(t)  (true)
+
+/**
+ * @brief FPD text display type validation check.
+ * @see dsFPDTextDisplay_t
+ */
+#define dsFPDTextDisplay_isValid(t)  (((t)  >= dsFPD_TEXTDISP_TEXT ) && ((t) < dsFPD_TEXTDISP_MAX))
+
+/**
+ * @brief Sleep mode validation check.
+ * @see dsSleepMode_t
+ */
+
+#define dsSleepMode_isValid(t)  (((t)  >= dsHOST_SLEEP_MODE_LIGHT) && ((t) < dsHOST_SLEEP_MODE_MAX))
+
+/**
+ * @brief Video screen zoom validation check.
+ * @see dsVideoZoom_t
+ */
+#define dsVideoPortDFC_isValid(t)  (((t) >= dsVIDEO_ZOOM_NONE ) && ((t) < dsVIDEO_ZOOM_MAX))
+
 /* End of DSHAL_UTILS doxygen group */
 /**
  * @}
@@ -91,6 +191,5 @@ while(0)
 
 #endif /* _DS_UTILS_H_ */
 
-/** @} */ // End of DSHAL_UTILS
-/** @} */ // End of DeviceSettings_Module
-/** @} */ // End of HPK
+/** @} */
+/** @} */
