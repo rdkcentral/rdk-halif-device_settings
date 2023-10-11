e87b89617dbbe6a961ac78c4eb116c5db255a58d
Prasad, Krishna (Software Engineer - Sky - Guest) <krishna.prasad2@sky.uk>
2023-10-11 10:25:24 +0100

    Update dsFPDSettings_template.h
    Signed-off-by: Prasad, Krishna (Software Engineer - Sky - Guest) <krishna.prasad2@sky.uk>

--- docs/pages/dsFPDSettings_template.h
+++ docs/pages/dsFPDSettings_template.h
@@ -174,7 +174,7 @@ const dsFPDTextDisplayConfig_t  kTextDisplays[dsFPD_TEXTDISP_MAX] = {
 		kIndicatorColors,		/**< supportedColors */
 		_MAX_BRIGHTNESS,		/**< maxBrightness */
 		_MAX_CYCLERATE,			/**< maxCycleRate */
-		"ABCDEFG",			/**< supportedCharacters */
+		"ABCDEFG",			/**< supportedCharacters (all alpha-numric characters except sprcial characters)*/
 		_MAX_HORZ_COLS,			/**< columns */
 		_MAX_VERT_ROWS,			/**< rows */
 		_MAX_HORZ_ITER,			/**< horizontal iterations */
