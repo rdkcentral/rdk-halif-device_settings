c1b71c1a51ae6d66dd31701db3eaeb9c0aba4cc0
Prasad, Krishna (Software Engineer - Sky - Guest) <krishna.prasad2@sky.uk>
2023-10-25 12:34:20 +0100

    Update dsVideoPortSettings_template.h
    Signed-off-by: Prasad, Krishna (Software Engineer - Sky - Guest) <krishna.prasad2@sky.uk>

--- docs/pages/dsVideoPortSettings_template.h
+++ docs/pages/dsVideoPortSettings_template.h
@@ -79,10 +79,10 @@ static const dsVideoPortType_t kSupportedPortTypes[] = { dsVIDEOPORT_TYPE_HDMI }
  */
 static const dsVideoPortTypeConfig_t kConfigs[]= {
 		{
-		/*.typeId = */						dsVIDEOPORT_TYPE_HDMI,
-		/*.name = */ 						"HDMI",
-		/*.dtcpSupported = */				false,
-		/*.hdcpSupported = */				true,
+		/*.typeId = */				dsVIDEOPORT_TYPE_HDMI,
+		/*.name = */ 				"HDMI",
+		/*.dtcpSupported = */			false,
+		/*.hdcpSupported = */			true,
 		/*.restrictedResollution = */		-1,
 		/*.numSupportedResolutions = */ 	dsUTL_DIM(kResolutions), // 0 means "Info available at runtime"
 		/*.supportedResolutons = */     	kResolutions,
@@ -94,9 +94,9 @@ static const dsVideoPortTypeConfig_t kConfigs[]= {
  */
 static const dsVideoPortPortConfig_t kPorts[] = {
 		{
-		/*.typeId = */ 					{dsVIDEOPORT_TYPE_HDMI, 0},
+		/*.typeId = */ 			{dsVIDEOPORT_TYPE_HDMI, 0},
 		/*.connectedAOP */              {dsAUDIOPORT_TYPE_HDMI, 0},
-		/*.defaultResolution = */		"720p"
+		/*.defaultResolution = */	"720p"
 		},
 };
 
