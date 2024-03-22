d6c9e750d02090ebbd93a53f407e03a69ac3ac14
krishna-1809 <150808219+krishna-1809@users.noreply.github.com>
2024-03-22 17:17:28 +0000

    gh #11 Description update for SetMixerLevels

--- include/dsAudio.h
+++ include/dsAudio.h
@@ -2069,7 +2069,7 @@ dsError_t dsGetHDMIARCPortId(int *portId);
 *
 * This function sets the mixer volume level for either primary(main audio) or system audio input(System Beep).
 *
-* @param[in] handle  - Handle for the output Audio port (Not Used as setting is not port specific)
+* @param[in] handle  - A valid handle refers to a specific audio port handle on the platform, or a NULL handle refers to use the current active port
 * @param[in] aInput  - dsAudioInputPrimary / dsAudioInputSystem. Please refer ::dsAudioInput_t
 * @param[in] volume  - volume to be set (0 to 100)
 *
@@ -2080,7 +2080,8 @@ dsError_t dsGetHDMIARCPortId(int *portId);
 * @retval dsERR_OPERATION_NOT_SUPPORTED    - The attempted operation is not supported; e.g: source devices
 * @retval dsERR_GENERAL                    - Underlying undefined platform error
 *
-* @pre dsAudioPortInit() and and dsGetAudioPort() must be called before calling this API
+* @pre dsAudioPortInit() should be called before calling this API
+*      dsGetAudioPort() should be called if a valid handle is used other than NULL
 *
 */
 dsError_t dsSetAudioMixerLevels (intptr_t handle, dsAudioInput_t aInput, int volume);
